d8d055f804299746b2acda485c046e537138a82c
rdeepc <dpr0413@gmail.com>
2016-12-15 02:06:40 +0600

    onclick

--- app/src/main/java/dp/ledtoggle/MainActivity.java
+++ app/src/main/java/dp/ledtoggle/MainActivity.java
@@ -3,6 +3,9 @@ package dp.ledtoggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
@@ -11,24 +14,54 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
     Gpio ledPin;
     String pinName="BCM4";
+    ToggleButton ledButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initial();
+
     }
 
     void initial(){
+        ledButton= (ToggleButton) findViewById(R.id.LEDtoggleButton);
         PeripheralManagerService service = new PeripheralManagerService();
         try {
 
+
          ledPin=service.openGpio(pinName);
          ledPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            ledOnOff();
 
         } catch (IOException e) {
             Log.e("LedToggle", "Error on PeripheralIO API", e);
         }
+    }
+
+    void ledOnOff(){
+        ledButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ledButton.isChecked()) {
+                    try {
+                        ledPin.setValue(!ledPin.getValue());
+                    Toast.makeText(MainActivity.this,"LED ON", Toast.LENGTH_SHORT).show();}
+                    catch (IOException e){
+                            Log.e("LedToggle", "Error on PeripheralIO API", e);}
+                    }
+                else {
+                    try {
+                        ledPin.setValue(!ledPin.getValue());
+                        Toast.makeText(MainActivity.this,"LED OFF", Toast.LENGTH_SHORT).show();}
+                    catch (IOException e){
+                        Log.e("LedToggle", "Error on PeripheralIO API", e);}
+                }
+
+            }
+        });
     }
 
 
