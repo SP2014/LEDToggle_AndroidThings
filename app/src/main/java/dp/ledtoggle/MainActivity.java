d320eb357e0ca3bee3bd657c138f38f193c41cdb
rdeepc <dpr0413@gmail.com>
2016-12-14 23:12:39 +0600

    led added

--- app/src/main/java/dp/ledtoggle/MainActivity.java
+++ app/src/main/java/dp/ledtoggle/MainActivity.java
@@ -2,6 +2,13 @@ package dp.ledtoggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void initial(){
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+         String pinName="BCM21";
+         Gpio ledPin=service.openGpio(pinName);
+         ledPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+        } catch (IOException e) {
+            Log.e("LedToggle", "Error on PeripheralIO API", e);
+        }
     }
 }
